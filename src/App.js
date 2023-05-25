2d3de54d9e26deb578b770045bc12afae19ebc3e
Shrikant-Wankhade <wshrikantp10@gmail.com>
2023-05-25 17:42:30 +0530

    added props to raw and hardcoded react components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ const Pet =(props)=> {
         "div",
         {},
         [  
-            React.createElement("h1", {}, "Luna"),
-            React.createElement("h2", {}, "Dog"),
-            React.createElement("h2", {}, "havanese")
+            React.createElement("h1", {}, props.name),
+            React.createElement("h2", {}, props.animal),
+            React.createElement("h2", {}, props.breed)
         ]
     ); 
 }
@@ -18,9 +18,9 @@ const App = ()=> {
         {},
         [
             React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet),
-            React.createElement(Pet),
-            React.createElement(Pet)
+            React.createElement(Pet,{name: "Luna", animal: "Dog", breed: "Havanese"}),
+            React.createElement(Pet,{name: "Pepper", animal: "Bird", breed: "Cockateil"}),
+            React.createElement(Pet,{name: "Doink", animal: "Cat", breed: "Mixed"})
         ]
     );
 };
